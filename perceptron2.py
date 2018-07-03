4502353cd38c53c90c5aa7347ebc9662a249966d
pedroalpacheco <pedro.pacheco.a@gmail.com>
2018-07-03 10:49:10 -0300

    Ebooks

--- perceptron2.py
+++ perceptron2.py
@@ -5,11 +5,14 @@ import numpy as np
 entradas = np.array([0, 1])
 pesos = np.array([0.5, 0.5])
 
+
 def soma(e, p):
     return e.dot(p)
 # dot product / produto escalar
-        
+
+
 s = soma(entradas, pesos)
+
 
 def stepFunction(soma):
     if (soma >= 1):
